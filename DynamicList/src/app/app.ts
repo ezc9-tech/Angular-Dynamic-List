3f76c9a8167c9f2e34cffd89bd72a2e1e60ebbb9
ezc9-tech <ezc9@outlook.com>
2026-01-11 12:37:26 -0600

    This is the project set up

---
+++ DynamicList/src/app/app.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  searchText: string = "";
+  items: string[] = [
+    'Angular Tutorial',
+    'React Tutorial',
+    'Vue.js Tutorial',
+    'JavaScript Basics',
+    'TypeScript Fundamentals',
+    'Building with HTML & CSS',
+    'Introduction to Node.js',
+    'Getting Started with MongoDB',
+    'Web Development with Node.js',
+    'Advanced JavaScript Concepts',
+    'Mastering Angular',
+    'Learning CSS Grid',
+    'Node.js for Beginners',
+    'The Complete JavaScript Guide',
+    'CSS Flexbox in Depth',
+    'Getting Started with Express.js',
+    'Deep Dive into GraphQL',
+    'Modern Web Development Trends',
+    'Building REST APIs with Express',
+    'Introduction to Git and GitHub',
+    'Web Accessibility Essentials',
+  ];
+}
